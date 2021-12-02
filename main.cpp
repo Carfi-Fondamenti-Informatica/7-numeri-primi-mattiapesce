b6f06301b3594dbdee8a3258febc92656c1fe048
mattiapesce <91664050+mattiapesce@users.noreply.github.com>
2021-12-02 07:57:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std ;
 
 int main(){
+  int numero;
+  cin >> numero;
+  if (numero==0) {
+    cout<< "numero primo" << endl;
+  }
+  else if(numero < 0){
   return 0;
+}
+else {
+  if (numeroPrimo (numero)== true ){
+    cout << "numero primo" << endl;
+  }
+  else {
+    cout << "numero non primo" << endl ;
+  }
+}
+  return o;
 }
