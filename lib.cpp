abe7a67c622e990679585be41ac8c02700b60179
mattiapesce <91664050+mattiapesce@users.noreply.github.com>
2021-12-02 07:52:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool numeroPrimo (int a ) {
+  bool primo = true ;
+  for (int i=2; i<a ; i++){
+    int risultato = a % i ;
+    if (risultato==0 and i<a){
+      primo = false ;
+      break ;
+    }
+  }
+  return primo ;
+}
